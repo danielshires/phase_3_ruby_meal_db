355c3d5ac52d08714751d2c735bf4286bf54a67c
danielshires <hello@danielshires.com>
2022-09-27 05:44:56 +1000

    First category test passed

--- lib/categories.rb
+++ lib/categories.rb
@@ -1,10 +1,8 @@
 require_relative "../config/environment.rb"
 
-class MealCategory
+class MealCategories
     
-    SEARCH_API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s="
-    DETAILS_API_URL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="
-    CATEGORY_API_URL = "https://themealdb.com/api/json/v1/1/filter.php?c="
+    CATEGORY_API_URL = "https://www.themealdb.com/api/json/v1/1/categories.php"
     
     def initialize(attributes)
         attributes.each do |key, value|
@@ -13,9 +11,9 @@ class MealCategory
         end
     end        
 
-    def self.filter_by_category(category)
-        url = CATEGORY_API_URL + category
-        make_request_and_parse_response(category)
+    def self.list_categories
+        url = CATEGORY_API_URL
+        make_request_and_parse_response(url)
     end
 
     private
@@ -27,14 +25,13 @@ class MealCategory
         response = Net::HTTP.get_response(uri)
         response_json = JSON.parse(response.body)
 
-        meals = response_json["meals"].collect do |meal|
+        meals = response_json["categories"].collect do |category|
             attributes = {
-                idCategory: meal["idCategory"],
-                strCategory: meal["strCategory"],
+                idCategory: category["idCategory"],
+                strCategory: category["strCategory"],
                 strCategoryDescription: "strCategoryDescription"
-          
             }
-            Meals.new(attributes)
+            MealCategories.new(attributes)
         end
 
     end
--- spec/meals_category_spec.rb
+++ spec/meals_category_spec.rb
@@ -1,6 +1,6 @@
 require_relative "./spec_helper"
 
-RSpec.describe MealCategory, "#initialize" do
+RSpec.describe MealCategories, "#initialize" do
     it "initializes a new object of the Meals class with the given attributes" do
 
         attributes = {
@@ -9,28 +9,18 @@ RSpec.describe MealCategory, "#initialize" do
             strCategoryDescription: "Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times.[1] Beef is a source of high-quality protein and essential nutrients.[2]"
         }
         
-        beef = MealCategory.new(attributes)
+        beef = MealCategories.new(attributes)
         
         expect(beef.strCategory).to eq "Beef"
     end
 end
 
-# RSpec.describe Meals, "#list_categories" do 
-#     it "fetches a list of meal categories based on an input" do
-#         meals = Meals.list_categories("beef")
-#         expect(meals.length).to be > 0
-#         meals.each do |meal|
-#             expect(meal).to be_a_kind_of Meals
-#         end
-#     end
-# end
-
-# RSpec.describe Meals, "#filter_by_category" do
-#     it "gets the details of the category given an string" do
-#         meals = Meals.filter_by_category("beef")
-#         expect(meals.length).to be > 0
-#         meals.each do |meal|
-#             expect(meal).to be_a_kind_of Meals
-#         end
-#     end
-# end
+RSpec.describe MealCategories, "#list_categories" do 
+    it "fetches a list of meal categories based on an input" do
+        meals = MealCategories.list_categories
+        expect(meals.length).to be > 0
+        meals.each do |meal|
+            expect(meal).to be_a_kind_of MealCategories
+        end
+    end
+end
