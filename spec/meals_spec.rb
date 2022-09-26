dd13b6bc758029342b6512cb6d9b5bee05bae450
danielshires <hello@danielshires.com>
2022-09-26 20:44:50 +1000

    Second test passed

--- spec/meals_spec.rb
+++ spec/meals_spec.rb
@@ -6,55 +6,53 @@ RSpec.describe Meals, "#initialize" do
         # Meaning that you set everything that is required for the test
         # What are the attributes for each method?
         attributes = {
-            id: "52771",
-            name: "Spicy Arrabiata Penne"
-            # strDrinkAlternate: nil,
-            # strCategory: "Vegetarian",
-            # strArea: "Italian",
-            # strInstructions: "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-            # strMealThumb: "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-            # strTags: "Pasta,Curry",
-            # strYoutube: "https://www.youtube.com/watch?v=1IszT_guI08",
-            # strIngredient1: "penne rigate",
-            # strIngredient2: "olive oil",
-            # strIngredient3: "garlic",
-            # strIngredient4: "chopped tomatoes",
-            # strIngredient5: "red chile flakes",
-            # strIngredient6: "italian seasoning",
-            # strIngredient7: "basil",
-            # strIngredient8: "Parmigiano-Reggiano",
-            # strIngredient9: "",
-            # strIngredient10: "" ,
-            # strIngredient11: "" ,
-            # strIngredient12: "" ,
-            # strIngredient13: "" ,
-            # strIngredient14: "" ,
-            # strIngredient15: "" ,
-            # strIngredient16: nil,
-            # strIngredient17: nil,
-            # strIngredient18: nil,
-            # strIngredient19: nil,
-            # strIngredient20: nil,
-            # strMeasure1: "1 pound",
-            # strMeasure2: "1/4 cup",
-            # strMeasure3: "3 cloves",
-            # strMeasure4: "1 tin ",
-            # strMeasure5: "1/2 teaspoon",
-            # strMeasure6: "1/2 teaspoon",
-            # strMeasure7: "6 leaves",
-            # strMeasure8: "spinkling",
-            # strMeasure9: "",
-            # strMeasure10: "",
-            # strMeasure11: "",
-            # strMeasure12: "",
-            # strMeasure13: "",
-            # strMeasure14: "",
-            # strMeasure15: "",
-            # strMeasure16: nil,
-            # strMeasure17: nil,
-            # strMeasure18: nil,
-            # strMeasure19: nil,
-            # strMeasure20: nil,
+            idMeal: "52771",
+            strMeal: "Spicy Arrabiata Penne",
+            strDrinkAlternate: nil,
+            strCategory: "Vegetarian",
+            strArea: "Italian",
+            strInstructions: "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+            strTags: "Pasta,Curry",
+            strIngredient1: "penne rigate",
+            strIngredient2: "olive oil",
+            strIngredient3: "garlic",
+            strIngredient4: "chopped tomatoes",
+            strIngredient5: "red chile flakes",
+            strIngredient6: "italian seasoning",
+            strIngredient7: "basil",
+            strIngredient8: "Parmigiano-Reggiano",
+            strIngredient9: "",
+            strIngredient10: "" ,
+            strIngredient11: "" ,
+            strIngredient12: "" ,
+            strIngredient13: "" ,
+            strIngredient14: "" ,
+            strIngredient15: "" ,
+            strIngredient16: nil,
+            strIngredient17: nil,
+            strIngredient18: nil,
+            strIngredient19: nil,
+            strIngredient20: nil,
+            strMeasure1: "1 pound",
+            strMeasure2: "1/4 cup",
+            strMeasure3: "3 cloves",
+            strMeasure4: "1 tin ",
+            strMeasure5: "1/2 teaspoon",
+            strMeasure6: "1/2 teaspoon",
+            strMeasure7: "6 leaves",
+            strMeasure8: "spinkling",
+            strMeasure9: "",
+            strMeasure10: "",
+            strMeasure11: "",
+            strMeasure12: "",
+            strMeasure13: "",
+            strMeasure14: "",
+            strMeasure15: "",
+            strMeasure16: nil,
+            strMeasure17: nil,
+            strMeasure18: nil,
+            strMeasure19: nil,
+            strMeasure20: nil,
         }
         # Act
         # Meaning you perform the action you want to test
@@ -62,6 +60,16 @@ RSpec.describe Meals, "#initialize" do
         # Assert
         # Meaning that you want to verify the whether the conditions meet the test
         # expect takes in the variable that you're testing
-        expect(spicy_arrabiata_penne.name).to eq "Spicy Arrabiata Penne"
+        expect(spicy_arrabiata_penne.strMeal).to eq "Spicy Arrabiata Penne"
+    end
+end
+
+RSpec.describe Meals, "#list_meals_starting_with" do 
+    it "fetches a list of meals given a alphabetical character" do
+        meals = Meals.list_meals_starting_with("b")
+        expect(meals.length).to be > 0
+        meals.each do |meal|
+            expect(meal).to be_a_kind_of Meals
+        end
     end
 end
